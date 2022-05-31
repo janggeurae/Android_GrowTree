1078725583e8dd82258f71435b9cbafe184fb139
janggeurae <472f57@gmail.com>
2022-06-01 01:00:47 +0900

    first commit

---
+++ app/src/main/java/com/example/mobile/mainGame.java
@@ -0,0 +1,19 @@
+package com.example.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class mainGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_game);
+    }
+
+    public void onClick(View v){
+        // jump
+    }
+}
